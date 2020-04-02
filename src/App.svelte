3ef1846fa8cea1918acfa9c8645a13b39da5a0e9
gloer <gloer@langslet.com>
2020-04-02 13:06:34 +0200

    Added chart

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,33 @@
 <script>
-	export let name;
+	import { onMount } from "svelte";
+	import Chart from "chart.js";
+	
+	function renderChart() {
+		var ctx = document.getElementById("myChart").getContext("2d");
+		var chart = new Chart(ctx, {
+		type: "line",
+		data: {
+			labels: ["January", "February", "March", "April", "May", "June", "July"],
+			datasets: [
+			{
+				label: "My First dataset",
+				backgroundColor: "rgb(255, 99, 132)",
+				borderColor: "rgb(255, 99, 132)",
+				data: [0, 10, 5, 2, 20, 30, 45]
+			}
+			]
+		},
+		options: {}
+		});
+	}
+
+	onMount(async () => {
+		renderChart()
+	});
+
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<canvas id="myChart"></canvas>
+	<button on:click={renderChart}>Renderr Chart</button>
 </main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
